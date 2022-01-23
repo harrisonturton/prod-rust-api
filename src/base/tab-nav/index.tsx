572b2aca2a7cd140cb767809e98bc381e1b722a8
Harrison Turton <harrisonturton@gmail.com>
2022-01-23 13:56:29 +1100

    Adjust tab nav styles

--- src/base/tab-nav/index.tsx
+++ src/base/tab-nav/index.tsx
@@ -2,10 +2,19 @@ import * as styles from "./styles.module.scss";
 
 const TabNav = () => (
     <div className={styles.root}>
-        <div>
-
-        </div>
+        <Tab/>
+        <Tab/>
+        <Tab/>
+        <Tab/>
+        <Tab/>
+        <Tab/>
     </div>
+);
+
+const Tab = () => (
+    <button className={styles.tab}>
+
+    </button>
 );
 
 export default TabNav;
