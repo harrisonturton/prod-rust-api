1e69ddf71856da6512ef51ced09da96450fb0a36
Harrison Turton <harrisonturton@gmail.com>
2022-01-31 20:52:45 +1100

    Add scaffolding for sign in form

--- web/src/views/home/index.tsx
+++ web/src/views/home/index.tsx
@@ -2,7 +2,45 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { getEditorRoute } from "base/routes";
+import { useState } from "react";
+import { TextField, Button } from "base/input";
 import styles from "./styles.module.scss";
+
+const Login = () => {
+    let [ username, setEmail ] = useState<string>("");
+    let [ password, setPassword ] = useState<string>("");
+    let [ loading, setLoading ] = useState<boolean>(false);
+    let [ error, setError ] = useState<string[]>([]);
+
+    const onSubmit = () => {
+        console.log("Submitted!");
+    };
+
+    return (
+        <div className={styles.formContainer}>
+            <span className={styles.signInLabel}>Sign in to your account</span>
+            <TextField
+                className={styles.inputField}
+                type="text"
+                placeholder="Email"
+                value={username}
+                onChange={setEmail}
+            />
+            <TextField
+                className={styles.inputField}
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={setPassword}
+            />
+            <Button
+                label="Sign In"
+                onClick={onSubmit}
+            />
+            {loading && <span>Loading</span>}
+        </div>
+    );
+};
 
 const Home: NextPage = () => (
     <div>
@@ -11,14 +49,10 @@ const Home: NextPage = () => (
             <meta name="description" content="Home page"/>
         </Head>
         <main className={styles.content}>
-            <span className={styles.homeLabel}>Jump to:</span>
-            <ul>
-                <li>
-                    <Link href={getEditorRoute()}>
-                        <a className={styles.homeLabel}>Editor</a>
-                    </Link>
-                </li>
-            </ul>
+            <Login/>
+            <Link href={getEditorRoute()}>
+                <a className={styles.homeLabel}>Don't have an account?</a>
+            </Link>
         </main>
     </div>
 );
