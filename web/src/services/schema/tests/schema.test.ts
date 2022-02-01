52a3f88fe08758b32afde9a458586d6d92c63d64
Harrison Turton <harrisonturton@gmail.com>
2022-02-01 12:37:26 +1100

    Add scaffolding for schema service tests

---
+++ web/src/services/schema/tests/schema.test.ts
@@ -0,0 +1,11 @@
+import { checkSchema } from "../schema";
+
+describe("checkSchema", () => {
+    it("rejects values that don't match schema", () => {
+        // TODO(harry): implement
+    });
+
+    it("accepts values that match schema", () => {
+        // TODO(harry): implement
+    });
+});
