b641d20d9645f59ac94b973d649098be5618263c
Harrison Turton <harrisonturton@gmail.com>
2022-02-09 01:51:12 +1100

    Create ecr repository

--- infra/main.tf
+++ infra/main.tf
@@ -1,24 +1,19 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.27"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
+  }
 
-    required_version = ">= 0.14.9"
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-    profile = "default"
-    region = "us-east-2"
+  profile = "default"
+  region  = "ap-southeast-2"
 }
 
-resource aws_instance "app_server" {
-    ami = "ami-830c94e3"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name = "AppServerInstance"
-    }
+resource "aws_ecr_repository" "editor_build_images" {
+  name = "editor_build_images"
 }
